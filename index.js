521658b9456719959131dfc36b61e2ef7759dfb0
Adam Brown <adam.brown.betty@gmail.com>
2019-06-26 15:10:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,31 +18,7 @@ var result = (a / b)
   return result;
 }
 
-var number = 10
- 
-function add5() {
-  number += 5
+function inc(n) {
+  var result = n+=
 }
- 
-function divideBy3() {
-  number /= 3
-}
- 
-divideBy3()
- 
-console.log(number) // 3.333333333335
- 
-add5()
- 
-console.log(number) // 8.333333333335
- 
-// reset number
-number = 10
- 
-add5()
- 
-console.log(number) // 15
- 
-divideBy3()
- 
-console.log(number) // 5
+
